e2942b2b8a987ac18fd51393fed42d04f3762d3e
vavisr01 <srikant.vavillapalli.ap@nielsen.com>
2017-10-30 08:44:34 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {FormsModule} from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products-list.component';
+import {ConvertToSpacePipe} from './shared/convert-to-space'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ConvertToSpacePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
