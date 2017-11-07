083a656ab5e09eb30637d77103c630c4f3acefb7
vavisr01 <srikant.vavillapalli.ap@nielsen.com>
2017-11-06 22:17:15 -0600

    changes for http

--- src/app/shared/proudct.service.ts
+++ src/app/shared/proudct.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "../products/products";
-import {HttpClient,HttpErrorResponse} from '@angular/common/http'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { Observable } from "rxjs/Observable";
 
 import 'rxjs/add/observable/throw';
@@ -11,20 +11,19 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class ProductService {
 
-    productsUrl:string='';
+    productsUrl: string = '';
 
-    constructor(private _http : HttpClient){
-        this.productsUrl='./api/products/products.json'
+    constructor(private _http: HttpClient) {
+        this.productsUrl = './api/products/products.json'
     }
 
     getProducts(): Observable<IProduct[]> {
-        return this._http.get<IProduct[]>(this._productUrl)
+        return this._http.get<IProduct[]>(this.productsUrl)
             .do(data => console.log('All: ' + JSON.stringify(data)))
             .catch(this.handleError);
     }
 
-   private handleError(err:HttpErrorResponse):Observable<> {
-        console.log('The error :'+ err.message);
-        Observable.throw(err.message);
+    private handleError(err: HttpErrorResponse): Observable<IProduct[]>{
+    return Observable.throw(err.message);
     }
 }
